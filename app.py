9697a51a750e23a47787e17354d5d17f43ff3d53
Matthew Pigram <Matthew>
2019-12-08 21:14:54 +0000

    updated image css

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+# creates the flask application
+app = Flask(__name__)
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+# Home page route
+@app.route('/')
+def home():
+    return render_template('index.html')
+def before_request():
+    app.jinja_env.cache = {}
+
+# run the application
+if __name__ == '__main__':
+    app.run(debug=True)
