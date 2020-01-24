ac523dfb13cb222b0509fde8ad3ec6095d4a4225
Matthew Pigram <Matthew>
2020-01-24 08:58:09 +0000

    updated jquery on mobile nav

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,17 +1,12 @@
-let mainNav = document.getElementById('js-menu');
-
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function() {
-  mainNav.classList.toggle('active');
+$(".navbar-toggle").click(function() {
+	$(".navbar-toggle").toggleClass("expand");
+	$(".main-nav").toggleClass("active");
 });
 
-//$(document).ready(function() {
-  //$('.nav-links').on('click', function() {
-    //$('.nav-links').hide();
+$(".nav-links").click(function() {
+	$(".main-nav").removeClass("active")
+});
 
-    //$('js-menu').on('click', function() {
-      //$('nav-links').removeClass('display:none;');
-   // });
- // });
-//});
+$(".logo").click(function() {
+	$(".main-nav").removeClass("active")
+});
