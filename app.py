2ab088b58b4359c318dc99565c3f64c282aa7ae5
pigstah <matthew.pigram2@gmail.com>
2020-03-05 09:57:07 +0000

    updated some files

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
 # creates the flask application
+
 app = Flask(__name__)
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # Home page route
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -13,5 +15,6 @@ def before_request():
     app.jinja_env.cache = {}
 
 # run the application
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
